18b06ae7f2f4e918cf20cc98a656e737e076ab8e
ulybu <ulybu@localhost.(none)>
2013-01-12 17:45:13 +0100

    Initial Commit

---
+++ Pry_Tsno_Proj/PTPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PTPAppDelegate.h
+//  Pry_Tsno_Proj
+//
+//  Created by ulybu on 12/01/13.
+//  Copyright (c) 2013 ulybu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PTPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pry_Tsno_Proj/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Pry_Tsno_Proj
+//
+//  Created by ulybu on 12/01/13.
+//  Copyright (c) 2013 ulybu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PTPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PTPAppDelegate class]));
+    }
+}
